4e4810d27c43ee2ea93cbba10d1f7afc48e40f0a
virat <virat@gmail.com>
2023-01-09 22:38:16 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+//import logo2.jpg from './images/'
 
 function App() {
   return (
@@ -6,8 +7,22 @@ function App() {
       <div class="main">
           <div class="container">
             <div class="container-sub-one">
-              <div class= "container-sub-one-left"></div>
-              <div class= "container-sub-one-right"></div>
+              <div class= "container-sub-one-left">
+                <img src="logo2.jpg" alt="quiz-app-logo" class="logo-image"/>
+              </div>
+              <div class= "container-sub-one-right">
+                <div class="container-sub-one-right-left">
+                  <h3>Quizilicious</h3>
+                </div>
+                <div class="container-sub-one-right-right">
+                  <div class="container-sub-one-right-right-one">
+                    <div class="container-sub-one-right-right-one-left">Share</div>
+                    <div class="container-sub-one-right-right-one-right">Contact</div>
+                  </div>
+                  <div class="container-sub-one-right-right-two"></div>
+                </div>
+                
+              </div>
             
             </div>
 
